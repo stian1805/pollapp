ea9b99997e30ddd5ff3bad76390a62d5d6eeacd3
Stian <stiansleilertsen@gmail.com>
2024-10-18 01:04:33 +0200

    added dockerfile

--- spring-demo-main/build.gradle.kts
+++ spring-demo-main/build.gradle.kts
@@ -19,8 +19,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+
 }
 
 tasks.withType<Test> {
